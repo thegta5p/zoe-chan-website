2db23d87e1330bdeadaf72a6e6e067dcea651c9e
Isaac Castro <74474132+thegta5p@users.noreply.github.com>
2023-09-17 14:39:14 -0700

    Created Home Page

---
+++ components/mainLayout.js
@@ -0,0 +1,31 @@
+import Styles from './layout.module.css';
+import Image from 'next/image';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+
+const SiteName = 'Zoe-Chan';
+
+export const siteTitle = 'Zoe-Chan Website';
+
+export default function MainLayout({children, home}) {
+  return (
+    <div className={Styles.container}>
+      <header className={Styles.header}>
+        {home ?(
+          <>
+            <h1 className={utilStyles.heading2Xl}>{SiteName}</h1>
+          </>
+        ):(
+        <>
+          <h2 className={utilStyles.headingLg}>
+            <Link href="/">
+              {SiteName}
+            </Link>
+          </h2>
+
+        </>)}
+      </header>
+    </div>
+  )
+}
