d91532bc9e5a0ed990b271c8654c1a61f18d878d
Isaac Castro <74474132+thegta5p@users.noreply.github.com>
2023-09-18 16:48:41 -0700

    Added a dark mode theme

---
+++ pages/ZoeLore/ZoeBiography.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import MainLayout from "../../components/mainLayout";
+
+export default function ZoeBiography() {
+  return (
+    <>
+      <MainLayout>
+
+      </MainLayout>
+
+      <div>
+        <div className="flex flex-col items-center">
+          <h1 className="font-bold text-2xl">Zoe</h1>
+          <h1>The Aspect of Twilight</h1>
+        </div>
+        
+
+      </div>
+
+    </>
+  )
+}
